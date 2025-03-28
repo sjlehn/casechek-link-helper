9015a5ee88528f65a350cd5b66ba20c6d809ae35
Steven Lehn <steven.lehn@casechek.com>
2025-03-28 15:09:36 -0500

    inital commit

---
+++ popup/popup.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', documentEvents  , false);
+
+
+function documentEvents() {
+    const links = {
+        platformLocal: "http://localhost:3200/api",
+        surgeryLocal: "http://localhost:3300/api",
+        paymentLocal: "http://localhost:3400/api",
+        catalogLocal: "http://localhost:3500/api",
+        hospitalLocal: "http://localhost:4200",
+        surgeryDev: "http://casechek-dev-surgery-service.casechek-test.com/api",
+        surgeryStaging: "http://casechek-staging-surgery-service.casechek-test.com/api",
+        platformDev: "http://casechek-dev-platform-management.casechek-test.com/api",
+        platformStaging: "http://casechek-staging-platform-management.casechek-test.com/api",
+        paymentDev: "http://casechek-dev-payment-service.casechek-test.com/api",
+        paymentStaging: "http://casechek-staging-payment-service.casechek-test.com/api",
+        hospitalDev: "http://hospital-dev.casechek-test.com/api",
+        hospitalStaging: "http://hospital-staging.casechek-test.com/api",
+        catalogDev: "http://casechek-dev-catalog-service.casechek-test.com/api",
+        catalogStaging: "http://casechek-staging-catalog-service.casechek-test.com/api",
+    };
+
+    document.getElementById('surgeryPr').addEventListener('click', () => {
+        const prNum = document.getElementById('prNum').value;
+    
+        chrome.tabs.create({
+            url: `http://casechek-pr-${prNum}-surgery-service.casechek-test.com/api`
+        })
+    })
+
+    document.getElementById('paymentPr').addEventListener('click', () => {
+        const prNum = document.getElementById('prNum').value;
+    
+        chrome.tabs.create({
+            url: `http://casechek-pr-${prNum}-payment-service.casechek-test.com/api`
+        })
+    })
+
+    document.getElementById('platformPr').addEventListener('click', () => {
+        const prNum = document.getElementById('prNum').value;
+    
+        chrome.tabs.create({
+            url: `http://casechek-pr-${prNum}-platform-management.casechek-test.com/api`
+        })
+    })
+
+    document.getElementById('catalogPr').addEventListener('click', () => {
+        const prNum = document.getElementById('prNum').value;
+    
+        chrome.tabs.create({
+            url: `http://casechek-pr-${prNum}-catalog-service.casechek-test.com/api`
+        })
+    })
+
+    document.getElementById('hospitalPr').addEventListener('click', () => {
+        const prNum = document.getElementById('prNum').value;
+    
+        chrome.tabs.create({
+            url: `https://hospital-pr-${prNum}.casechek-test.com/`
+        })
+    })
+
+    for (const [id, url] of Object.entries(links)) {
+        document.getElementById(id).addEventListener('click', () => {
+            chrome.tabs.create({
+                url: url
+            })
+        })
+    }
+}
